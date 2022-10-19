fb8639a819b46de62b4a24862e13e0b158541b1e
TiepHoang23 <89854924+TiepHoang23@users.noreply.github.com>
2022-10-19 13:43:33 +0700

    fix app

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 const express = require('express');
-// const { resolve } = require('path');
-const path = require('path')
-const app = express()
-const port = process.env.PORT|| 5000
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 3000;
 app.use(express.static('build'));
 app.get('*', (req, res) => {
-    req.sendFile(path,resolve(__dirname,'build','index.html'));
+  req.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
-app.listen(port, 
-    (err) => {
-        if(err) throw err;
-     console.log(`Example app listening on port ${port}!`);
-    })
-    
+app.listen(port, (err) => {
+  if (err) throw err.message;
+  console.log(`Server running on port${port}`);
+});
